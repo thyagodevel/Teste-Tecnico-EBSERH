fcd733f558a5b18283059577069804120b3b4d6b
Thyago Cabral <thyagoc.desousa@gmail.com>
2026-07-15 12:51:56 -0300

    feat: configuração inicial do projeto e estrutura de arquitetura
    - Configura estrutura base do projeto
    - Define organização de pacotes/camadas da arquitetura
    - Adiciona configurações essenciais (build, dependências, ambiente)

---
+++ src/main/java/br/com/teste/ebserh/paciente_api_ebserh/PacienteApiEbserhApplication.java
@@ -0,0 +1,13 @@
+package br.com.teste.ebserh.paciente_api_ebserh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacienteApiEbserhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacienteApiEbserhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/teste/ebserh/paciente_api_ebserh/PacienteApiEbserhApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste.ebserh.paciente_api_ebserh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacienteApiEbserhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
